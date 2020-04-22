5b775a0c5bf5fb9689f1d5834ee36b7bf0a532df
jm010426 <chuan.he@baozun.com>
2020-04-22 17:47:37 +0800

    Initial commit

---
+++ src/main/java/com/hechuan/mapstrutdemo/MapstrutDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hechuan.mapstrutdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstrutDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstrutDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hechuan/mapstrutdemo/MapstrutDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hechuan.mapstrutdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstrutDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
